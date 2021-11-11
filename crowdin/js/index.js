8c4729f678d1319d934a5bd31757073cc82a1d1b
subbuswaroop <subbuswaroop3795@gmail.com>
2021-11-11 21:52:29 +0530

    For download

--- crowdin/js/index.js
+++ crowdin/js/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+const AdmZip = require('adm-zip');
 /** CONFIG ENV VARIABLES */
 // const BASE_URL = config.HOST + config.NAMESPACE + "projects/";
 // const PROJECT_ID = config.PROJECT_ID;
@@ -27,7 +28,7 @@ function checkBuildStatus(buildId) {
         return status;
       } else if(status == "finished") {
         clearInterval(checkBuilStatusInterval);
-        downloadTranslations();
+        getDownloadURL(buildId);
       }
       return status;
     }
@@ -45,13 +46,51 @@ function buildTranslations() {
   return axios(options).then(response=> {
     if(response.data && response.data.data) {
       let buildId = response.data.data.id;
+      // The build process usually takes more than a minute due to large number of keys. So, we are only proceeding
+      // to the next step if this build finishes. Until then, we check the status every 30 seconds
       checkBuilStatusInterval = setInterval(checkBuildStatus, 5000, buildId);
     }
   })
 }
 
-function downloadTranslations() {
+function getDownloadURL(buildId) {
   console.log("Coming to Download");
+  let zipURL = "";
+  const options = {
+    method: "GET",
+    ...HEADERS,
+    url: `https://api.crowdin.com/api/v2/projects/${CROWDIN_PROJECT_ID}/translations/builds/${buildId}/download`
+  };
+
+  axios(options).then(response=> {
+    if(response.data) {
+      zipURL = response.data.url;
+      downloadTranslations(zipURL);
+    }
+  })
+  // `https://api.crowdin.com/api/v2/projects/${CROWDIN_PROJECT_ID}/languages/${languageId}/translations`
+
+}
+
+function downloadTranslations(zipURL) {
+  const options = {
+    method: "GET",
+    responseType: "arraybuffer",
+    ...HEADERS,
+    url: zipURL
+  };
+
+  axios(options).then(response=> {
+    if(response && response.data) {
+      var zip = new AdmZip(response.data);
+      var zipEntries = zip.getEntries();
+      console.log(zipEntries.length);
+    
+      zipEntries.forEach((entry) => {
+        console.log(zip.readAsText(entry));
+      });
+    }
+  })
 }
 
 buildTranslations();
