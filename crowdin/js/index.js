a3c9d426a703c8284f136593c11331010dc28663
subbuswaroop <subbuswaroop3795@gmail.com>
2021-11-11 19:21:15 +0530

    For PR, download

--- crowdin/js/index.js
+++ crowdin/js/index.js
@@ -5,12 +5,34 @@ const axios = require('axios').default;
 
 const CROWDIN_TOKEN = process.argv[3];
 const CROWDIN_PROJECT_ID = process.argv[2];
-
+var checkBuilStatusInterval = null;
 const HEADERS = {
   headers: {
     'Authorization': `Bearer ${CROWDIN_TOKEN}`
   }
 };
+
+function checkBuildStatus(buildId) {
+  const options = {
+    method: "GET",
+    ...HEADERS,
+    url: `https://api.crowdin.com/api/v2/projects/${CROWDIN_PROJECT_ID}/translations/builds/${buildId}`
+  };
+
+  return axios(options).then(response=> {
+    if(response.data && response.data.data) {
+      const status = response.data.data.status;
+      console.log("Status:: ", status);
+      if(status == "inProgress") {
+        return status;
+      } else if(status == "finished") {
+        clearInterval(checkBuilStatusInterval);
+        downloadTranslations();
+      }
+      return status;
+    }
+  })
+}
 
 function buildTranslations() {
   const options = {
@@ -22,9 +44,14 @@ function buildTranslations() {
 
   return axios(options).then(response=> {
     if(response.data && response.data.data) {
-      console.log(response.data.data);
+      let buildId = response.data.data.id;
+      checkBuilStatusInterval = setInterval(checkBuildStatus, 5000, buildId);
     }
   })
+}
+
+function downloadTranslations() {
+  console.log("Coming to Download");
 }
 
 buildTranslations();
