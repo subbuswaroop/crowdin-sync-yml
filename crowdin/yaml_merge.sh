3f42e0498721a740340bd22fb7ef4f2bb91cfd05
subbuswaroop <subbuswaroop3795@gmail.com>
2021-11-15 19:45:50 +0530

    Local changes

--- crowdin/yaml_merge.sh
+++ crowdin/yaml_merge.sh
@@ -17,4 +17,4 @@ command -v yaml-merge >/dev/null 2>&1 || install_yaml_merge
 yaml-merge "${source_dir}/$2.yml" "./tmp/$tmp_file" | sed -e 's/\\_/ /g' > "${source_dir}/$1"
 echo "copied over ${tmp_file}"
 
-rm "$tmp_file"
+rm "./tmp/$tmp_file"
